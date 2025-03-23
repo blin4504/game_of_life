f6c41a8c704a303c084aa86626aa359c7e6cb11c
blin4504 <bl4504@g.rit.edu>
2025-03-23 00:36:52 -0400

    feat: finish game of life

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::vec;
+use std::{thread, time::Duration};
+
 #[derive(Debug, Clone)]
 struct Cell {
     alive: bool,
@@ -6,12 +9,12 @@ struct Cell {
 struct Board {
     height: usize,
     width: usize,
-    board: Vec<Vec<Cell>>
+    board: Vec<Vec<Cell>>,
 }
 
 impl Board {
     fn new(height: usize, width: usize) -> Self {
-        let default_cell = Cell { alive: false};
+        let default_cell = Cell { alive: false };
         Self {
             width: width,
             height: height,
@@ -20,21 +23,82 @@ impl Board {
     }
 
     fn display(&self) {
-        for x in 0..self.height {
+        print!("\x1B[2J\x1B[1;1H");
+        for y in 0..self.height {
             print!("\n");
-            for y in 0..self.width {
+            for x in 0..self.width {
                 if !self.board[x][y].alive {
                     print!("_ ");
                 } else {
                     print!("* ");
                 }
-            } 
+            }
         }
+    }
+
+    fn spawn(&mut self, x: usize, y: usize) {
+        if y < self.height && x < self.width {
+            self.board[x][y].alive = true;
+        }
+    }
+
+    fn update(&mut self) {
+        let mut new_board: Vec<Vec<Cell>> = self.board.clone();
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let neighbor_cnt = count_neighbors(self, x, y);
+
+                if !self.board[x][y].alive {
+                    if neighbor_cnt == 3 {
+                        new_board[x][y].alive = true;
+                    }
+                } else {
+                    if neighbor_cnt < 2 || neighbor_cnt > 3 {
+                        new_board[x][y].alive = false;
+                    }
+                }
+            }
+        }
+        self.board = new_board;
     }
 }
 
+fn count_neighbors(board: &mut Board, x: usize, y: usize) -> i32 {
+    let directions: [(isize, isize); 8] = [
+            (0, 1),
+            (0, -1),
+            (1, 0),
+            (-1, 0),
+            (1, 1),
+            (1, -1),
+            (-1, 1),
+            (-1, -1),
+        ];
+    let mut neighbor_cnt = 0;
+    for (dx, dy) in directions {
+        let nx = x as isize + dx;
+        let ny = y as isize + dy;
+        if nx < 0 || nx >= board.width as isize || ny < 0 || ny >= board.height as isize {
+            continue;
+        }
+        if board.board[nx as usize][ny as usize].alive {
+            neighbor_cnt += 1;
+        }
+    }
+    return neighbor_cnt
+}
+
 fn main() {
-    let board = Board::new(10, 10);
-    board.display();
-    print!("hello");
+    let mut board = Board::new(10, 10);
+    board.spawn(1, 1);
+    board.spawn(2, 2);
+    board.spawn(3, 2);
+    board.spawn(3, 1);
+    board.spawn(3, 0);
+
+    // loop {
+        board.display();
+        board.update();
+        // thread::sleep(Duration::from_millis(1000));
+    // }    
 }
